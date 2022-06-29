71aaed515bf3d61e393b1ea214690ea2cf2e32a3
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-28 23:25:13 -0700

    Move index.js to lib/ and update project configuration

--- test/lib/patch.js
+++ test/lib/patch.js
@@ -1,6 +1,6 @@
 if (!Buffer.from) {
   Buffer.from = function bufferFrom(data, encoding) {
-    // eslint-disable-next-line node/no-deprecated-api
+    // eslint-disable-next-line n/no-deprecated-api
     return new Buffer(data, encoding);
   };
 }

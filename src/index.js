4991e3b8219af96c850ca0e08ae2aef168e64df7
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-17 13:35:11 -0800

    Fixed exports

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require('./polyfills');
-
-const BaseIterator = require('extract-base-iterator').default;
-
-module.exports = require('./ZipIterator');
-module.exports.DirectoryEntry = BaseIterator.DirectoryEntry;
-module.exports.FileEntry = require('./FileEntry');
-module.exports.LinkEntry = BaseIterator.LinkEntry;
-module.exports.SymbolicLinkEntry = BaseIterator.SymbolicLinkEntry;

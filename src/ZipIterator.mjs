c80c1dbb5e3932b19c3d193c263aa0d297bdc713
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-18 09:13:10 -0800

    esm version

--- src/ZipIterator.mjs
+++ src/ZipIterator.mjs
@@ -1,69 +1,69 @@
-import './polyfills.cjs';
 import fs from 'fs';
 import path from 'path';
+import './polyfills.cjs';
 
 import BaseIterator from 'extract-base-iterator';
 import Queue from 'queue-cb';
 import shortHash from 'short-hash';
 import tempSuffix from 'temp-suffix';
 
-import nextEntry from './nextEntry.mjs';
-import fifoRemove from './lib/fifoRemove.mjs';
-import Zip from './lib/Zip.mjs';
 import Lock from './lib/Lock.mjs';
+import Zip from './lib/Zip.mjs';
+import fifoRemove from './lib/fifoRemove.mjs';
 import streamToFile from './lib/streamToFile.mjs';
+import nextEntry from './nextEntry.mjs';
 
 import os from 'os';
 import osShim from 'os-shim';
 
-const tmpdir  = os.tmpdir || osShim.tmpdir;
+const tmpdir = os.tmpdir || osShim.tmpdir;
 
-export default class ZipIterator extends BaseIterator{
-  constructor (source, options) {
-  super(options);
-  this.lock = new Lock();
-  this.lock.iterator = this;
+export default class ZipIterator extends BaseIterator {
+  constructor(source, options) {
+    super(options);
+    this.lock = new Lock();
+    this.lock.iterator = this;
 
-  const queue = Queue(1);
-  let cancelled = false;
-  function setup() {
-    cancelled = true;
-  }
-  this.processing.push(setup);
+    const queue = Queue(1);
+    let cancelled = false;
+    function setup() {
+      cancelled = true;
+    }
+    this.processing.push(setup);
 
-  if (typeof source !== 'string') {
-    this.lock.tempPath = path.join(tmpdir(), 'zip-iterator', shortHash(process.cwd()), tempSuffix('tmp.zip'));
-    queue.defer(streamToFile.bind(null, source, this.lock.tempPath));
-  }
+    if (typeof source !== 'string') {
+      this.lock.tempPath = path.join(tmpdir(), 'zip-iterator', shortHash(process.cwd()), tempSuffix('tmp.zip'));
+      queue.defer(streamToFile.bind(null, source, this.lock.tempPath));
+    }
 
-  // open zip
-  queue.defer((callback) => {
-    fs.open(this.lock.tempPath || source, 'r', '0666', (err, fd) => {
-      if (this.done || cancelled) return; // done
-      if (err) return callback(err);
-      const reader = new Zip(fd);
-      this.lock.fd = fd;
-      this.iterator = reader.iterator();
-      callback();
+    // open zip
+    queue.defer((callback) => {
+      fs.open(this.lock.tempPath || source, 'r', '0666', (err, fd) => {
+        if (this.done || cancelled) return; // done
+        if (err) return callback(err);
+        const reader = new Zip(fd);
+        this.lock.fd = fd;
+        this.iterator = reader.iterator();
+        callback();
+      });
     });
-  });
 
-  // start processing
-  queue.await((err) => {
-    fifoRemove(this.processing, setup);
-    if (this.done || cancelled) return; // done
-    err ? this.end(err) : this.push(nextEntry);
-  });
-}
-
-end(err) {
-  if (this.lock) {
-    this.lock.err = err;
-    this.lock.release();
-    this.lock = null;
-  } else {
-    BaseIterator.prototype.end.call(this, err); // call in lock release so end is properly handled
+    // start processing
+    queue.await((err) => {
+      fifoRemove(this.processing, setup);
+      if (this.done || cancelled) return; // done
+      err ? this.end(err) : this.push(nextEntry);
+    });
   }
-  this.iterator = null;
-};
+
+  end(err) {
+    if (this.lock) {
+      this.lock.err = err;
+      this.lock.release();
+      this.lock = null;
+    } else {
+      BaseIterator.prototype.end.call(this, err); // call in lock release so end is properly handled
+    }
+    this.iterator = null;
+  }
 }

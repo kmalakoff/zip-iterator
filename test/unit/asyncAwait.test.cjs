64196696aa6f70710733a3f219dd7427b89565bd
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 14:18:31 -0800

    Upgrade modules

--- test/unit/asyncAwait.test.cjs
+++ test/unit/asyncAwait.test.cjs
@@ -43,14 +43,18 @@ async function extractForEach(iterator, dest, options) {
 
 describe('asyncAwait', () => {
   if (typeof Symbol === 'undefined' || !Symbol.asyncIterator) return;
-  let globalPromise;
-  before(() => {
-    globalPromise = global.Promise;
-    global.Promise = require('pinkie-promise');
-  });
-  after(() => {
-    global.Promise = globalPromise;
-  });
+  (() => {
+    // patch and restore promise
+    const root = typeof global !== 'undefined' ? global : window;
+    let rootPromise;
+    before(() => {
+      rootPromise = root.Promise;
+      root.Promise = require('pinkie-promise');
+    });
+    after(() => {
+      root.Promise = rootPromise;
+    });
+  })();
 
   beforeEach((callback) => {
     rimraf2(TMP_DIR, { disableGlob: true }, () => {
